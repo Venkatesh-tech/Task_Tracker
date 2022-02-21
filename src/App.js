904d6b45d46dbdf1a2cd16eb2891632d5c16311d
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-02-21 22:35:45 +0530

    Added headerjs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "Venkatesh";
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello from React</h1>
+      <h2>Hello from {name}</h2>
     </div>
   );
 }
