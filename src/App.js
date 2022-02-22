809bd8a2337eddde81e2335fdb9a7f6c3fa5cfd4
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-02-22 18:43:16 +0530

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  const name = "Venkatesh";
+  //const name = "Venkatesh";
 
   return (
     <div className="container">
-      <Header />
+      <Header title="Hello" />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import Header from "./Header";
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   );
 };
